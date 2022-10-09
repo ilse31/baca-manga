08585557cd4996b8cd3e98df3f6456a93223ffe9
ilse31 <ilhamprazsetyo33@gmail.com>
2022-10-09 15:48:30 +0700

    add gin

--- main.go
+++ main.go
@@ -1,5 +1,29 @@
 package main
 
+import (
+	"log"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+)
+
 func main() {
 
+	router := InitRoute()
+	log.Fatal(router.Run(":8080"))
+
+}
+
+func InitRoute() *gin.Engine {
+	router := gin.Default()
+
+	router.Use(gin.Logger())
+	router.Use(gin.Recovery())
+	router.Use(cors.Default())
+	router.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	return router
 }
