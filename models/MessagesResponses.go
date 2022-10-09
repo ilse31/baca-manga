1d2c0aa6ccab0edd524d3ab26d2d89db5bf5f221
ilse31 <ilhamprazsetyo33@gmail.com>
2022-10-09 16:16:03 +0700

    Fiks Responses

--- models/MessagesResponses.go
+++ models/MessagesResponses.go
@@ -3,14 +3,17 @@ package models
 type Error struct {
 	Code    int    `json:"code"`
 	Message string `json:"message"`
+	Status  string `json:"status"`
 }
 
 type Success struct {
-	Code    int    `json:"code"`
-	Message string `json:"message"`
+	Code    int         `json:"code"`
+	Message string      `json:"message"`
+	Data    interface{} `json:"data"`
 }
 
 type PingSuccess struct {
 	Code    int    `json:"code"`
 	Message string `json:"message"`
+	Status  string `json:"status"`
 }
--- models/Responses.go
+++ models/Responses.go
@@ -9,3 +9,24 @@ type DetailResponses struct {
 	Url      string `json:"url"`
 	Endpoint string `json:"endpoint"`
 }
+
+type ListManga struct {
+	Name      string `json:"name"`
+	Thumbnail string `json:"thumbnail"`
+	Url       string `json:"url"`
+	Endpoint  string `json:"endpoint"`
+}
+
+type ListResponsesManga struct {
+	Manga      []ListManga       `json:"manga"`
+	Pagination []DetailResponses `json:"pagination"`
+}
+
+type PopularManga struct {
+	Name        string            `json:"name"`
+	Thumbnail   string            `json:"thumbnail"`
+	Url         string            `json:"url"`
+	Endpoint    string            `json:"endpoint"`
+	LastUpload  string            `json:"last_upload"`
+	LastChapter []DetailResponses `json:"last_chapter"`
+}
