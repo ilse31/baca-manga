bc6ba4ab25f085aed1315566a6f03534d1086977
Bejjo <regie@Brilians-MacBook-Pro.local>
2022-10-11 17:31:13 +0700

    add route pokedex

--- routes/routes.go
+++ routes/routes.go
@@ -10,4 +10,5 @@ func Routes(router *gin.RouterGroup) {
 	router.GET("/ping", controllers.Ping)
 	router.GET("/home", controllers.Home)
 	router.GET("/ivan", controllers.ControllerIvan)
+	router.GET("/pokedex", controllers.BejjoController)
 }
