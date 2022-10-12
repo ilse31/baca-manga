8a1d74751ff51617dc3e27a9f6704aa412f83af9
ivan <ivannovaa.24@gmail.com>
2022-10-12 11:50:12 +0700

    add new struct

--- response/IvanResponse.go
+++ response/IvanResponse.go
@@ -16,3 +16,15 @@ type PingIvanSuccess struct {
 	Message    string `json:"message"`
 	StatusCode int    `json:"statusCode"`
 }
+
+type Pokedex struct {
+	Id     	   string          `json:"id"`
+	Name       string       `json:"name"`
+	Total	   string		`json:"total"`
+	HP 	   string    	`json:"hp"`
+	Attack	   string    	`json:"attack"`
+	Defense	   string    	`json:"defense"`
+	Speed      string          `json:"speed"`
+	SpAttack      string          `json:"spattack"`
+	SpDefense      string          `json:"spdefense"`
+}
