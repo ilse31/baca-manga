09982ea3bf753666f56cd901471182aefb1801f0
WhataD?0wD <ditakusuma66@gmail.com>
2022-10-22 19:36:15 +0700

    push-1

---
+++ response/Dissleeping.go
@@ -0,0 +1,8 @@
+package response
+
+type Dissleeping struct {
+	Status     string `json:"status"`
+	Message    string `json:"message"`
+	StatusCode string `json:"statusCode"`
+	Nama       string `json:"nama"`
+}
