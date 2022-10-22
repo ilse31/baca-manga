bfd66ddc83685b1dbb30f18316d12a76d812aa80
Ardi Handoyo <handomukti233@gmail.com>
2022-10-22 18:46:13 +0700

    AddGenre

--- response/itsukahan.go
+++ response/itsukahan.go
@@ -5,6 +5,7 @@ type ItsukaHandoko struct {
 	Message    string `json:"message"`
 	StatusCode int    `json:"statusCode"`
 	Nama       string `json:"nama"`
+	Genre	   string `json:"genre"`
 }
 
 type ItsukaSuccess struct {
