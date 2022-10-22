5fadf26953f9d204c39d6a111ed397297be2833a
WhataD?0wD <ditakusuma66@gmail.com>
2022-10-22 20:24:20 +0700

    push-4

--- response/Dissleeping.go
+++ response/Dissleeping.go
@@ -6,3 +6,22 @@ type Dissleeping struct {
 	StatusCode string `json:"statusCode"`
 	Nama       string `json:"nama"`
 }
+
+type DissleepingResponses struct {
+	Title      string   `json:"title"`
+	Thumbnail  string   `json:"thumbnail"`
+	Alter      string   `json:"alter"`
+	Status     string   `json:"status"`
+	Author     string   `json:"author"`
+	Ilustrator string   `json:"ilustrator"`
+	Grafis     string   `json:"grafis"`
+	Genre      []string `json:"genre"`
+	Synopsis   string   `json:"synopsis"`
+	Chapter    []string `json:"chapter"`
+}
+
+type KomikActionChapter struct {
+	Title  string   `json:"title"`
+	Thumb  string   `json:"thumb"`
+	Images []string `json:"images"`
+}
