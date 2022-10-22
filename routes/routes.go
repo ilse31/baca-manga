4d4506fe2d145e54a7fb58b91bf86b1de7d1b27e
WhataD?0wD <ditakusuma66@gmail.com>
2022-10-22 19:56:24 +0700

    push-3

--- routes/routes.go
+++ routes/routes.go
@@ -18,5 +18,5 @@ func Routes(router *gin.RouterGroup) {
 	router.GET("/villaneuvo", controllers.ControllerVillaneuvo)
 	router.GET("/itsuka", controllers.ControllerHandoko)
 	router.GET("/khoeru", controllers.ControllerKhoeru)
-	router.GET("/Dissleeping", controllers.ControllerDiissleeping)
+	router.GET("/Dissleeping", controllers.ControllerDissleeping)
 }
